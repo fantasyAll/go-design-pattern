87b896a37d3c696c948d72844b73d85c5e94762a
jesseChen <jesse96@foxmail.com>
2020-11-15 17:46:06 +0800

    单例模式练习测试

--- singleton/lazy/singleton_th_test.go
+++ singleton/lazy/singleton_th_test.go
@@ -1,6 +1,9 @@
 package lazy
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestGetSingleton_th(t *testing.T) {
 	s1 := GetSingleton_th()
@@ -13,5 +16,34 @@ func TestGetSingleton_th(t *testing.T) {
 	t.Logf("s2：%p %T", s2, s2)
 	if s1 != s2 {
 		t.Fatal("不一致！")
+	}
+}
+
+// 测试并行声明
+func TestParallelSingleton(t *testing.T) {
+	// 控制获取实例化开始
+	start := make(chan struct{})
+	// 控制获取实例化结束
+	wg := sync.WaitGroup{}
+	wg.Add(parCount)
+	instances := [parCount]*singleton_th{}
+	for i := 0; i < parCount; i++ {
+		go func(index int) {
+			<-start
+			instances[index] = GetSingleton_th()
+			wg.Done()
+		}(i)
+	}
+	// 关闭chan开始获取实例化
+	close(start)
+	// 阻塞
+	wg.Done()
+	// 判断所有的实例是否一致
+	for i := 1; i < parCount; i++ {
+		if instances[i] != instances[i-1] {
+			t.Logf("i：%d %p", i, instances[i])
+			t.Logf("i-1：%d %p", i-1, instances[i-1])
+			t.Fatal("不一致！")
+		}
 	}
 }
